c4d2eed880b73c0ee88d799c097eea5f777b3ae9
FirminoVinicius <firmino.vinicius@usp.br>
2025-08-29 15:31:38 -0300

    scripts for analysis

---
+++ scripts/anisotropy.py
@@ -0,0 +1,116 @@
+import MDAnalysis as mda
+import numpy as np
+import pandas as pd
+
+def calculate_angle(vec1, vec2):
+
+    dot_product = np.inner(vec1, vec2)
+
+    # Calculate the magnitudes (norms) of the vectors
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+
+    # Calculate the cosine of the angle
+    cos_angle = dot_product / (norm_vec1 * norm_vec2)
+
+    # Calculate the angle in radians
+    angle_radians = np.arccos(cos_angle)
+
+    # Convert the angle to degrees
+    #angle_degrees = np.degrees(angle_radians)
+
+    return angle_radians
+
+def transition_dipole(trp):
+    #Defines the transitions dipoles as rotations of the long axis vector
+    #about the normal vector of the indol plane with origin in the COG of the indol group
+
+    indol = trp.select_atoms("name NE1 CD1 CG CD2 CE2 CE3 CZ3 CH2 CZ2")
+    COG = indol.center_of_geometry()
+
+    #The vector goes from cd1 to the COG
+    #Translate to origin
+
+    cd1 = trp.select_atoms("name CD1")
+    long_axis = cd1.positions-COG
+
+    #Define normal do the indol group
+    #First find two vectors in the indol plane
+
+    cz3 = trp.select_atoms("name CZ3")
+    ch2 = trp.select_atoms("name CH2")
+    plane_vector1 = cz3.positions - COG
+    plane_vector2 = ch2.positions - COG
+
+    #take cross product and normalize
+
+    normal = np.cross(plane_vector1, plane_vector2)
+    normal = normal/np.linalg.norm(normal)
+
+    #Rotate the long axis vector to give the two trasition dipoles using the Rodrigues formula
+
+    angle1 = np.deg2rad(-38)
+    angle2 = np.deg2rad(54)
+
+    La = long_axis*np.cos(angle1) \
+      + np.cross(normal, long_axis)*np.sin(angle1) \
+      + normal*(np.inner(normal, long_axis))*(1-np.cos(angle1))
+
+    Lb = long_axis*np.cos(angle2) \
+      + np.cross(normal, long_axis)*np.sin(angle2) \
+      + normal*(np.inner(normal, long_axis))*(1-np.cos(angle2))
+
+    return La, Lb
+
+def separation_vector(trp1, trp2):
+    #calculates de separation vector between the two indol groups
+
+    indol1 = trp1.select_atoms("name NE1 CD1 CG CD2 CE2 CE3 CZ3 CH2 CZ2")
+    indol2 = trp2.select_atoms("name NE1 CD1 CG CD2 CE2 CE3 CZ3 CH2 CZ2")
+
+    sep_vector = indol2.center_of_geometry() - indol1.center_of_geometry()
+
+    return sep_vector
+
+#Load trajectory and topology
+
+topology_file = '../input/conf.pdb'
+trajectory_file = 'center.xtc'
+u = mda.Universe(topology_file, trajectory_file)
+
+TRP_donnor = u.select_atoms("chainID A and resname TRP")
+TRP_acceptor = u.select_atoms("chainID B and resname TRP")
+
+timeseries = []
+# vectors_donnor = transition_dipole(TRP_donnor)
+
+for ts in u.trajectory:
+
+  vectors_donnor = transition_dipole(TRP_donnor)
+  vectors_acceptor = transition_dipole(TRP_acceptor)
+
+  # Angle between trasition moments
+  angle_trans = calculate_angle(vectors_donnor[0], vectors_acceptor[0])
+
+  # Angles between trasition moments and separation vector
+  sep_vec = separation_vector(TRP_donnor, TRP_acceptor)
+  angle_Rd = calculate_angle((vectors_donnor[0]), sep_vec)
+  angle_Ra = calculate_angle((vectors_acceptor[0]), sep_vec)
+
+  # Orientation factor
+  k = np.cos(angle_trans) - (3 * np.cos(angle_Rd) * np.cos(angle_Ra))
+
+  # Depolarization
+  depol = 0.18*((1.5*np.power(np.cos(angle_trans),2)) - 0.5)
+  # depol = 0.18 - depol
+
+  timeseries.append([ts.frame, k[0][0]*k[0][0], np.linalg.norm(sep_vec), depol[0][0], angle_trans[0][0], angle_Rd[0], angle_Ra[0]])
+
+  data = np.array(timeseries, dtype="object")
+
+data_df = pd.DataFrame(data, columns=['Frame','Orientation factor', 'COG distance', 'Depolarization', 'angle_trans', 'angle_Rd', 'angle_Ra'])
+
+t = pd.DataFrame(data_df, columns=['COG distance', 'Orientation factor', 'angle_trans', 'angle_Rd', 'angle_Ra'])
+t.to_csv('aniso-La-La.dat', index=False, sep=' ')
+
+
---
+++ scripts/monomer_orientation.py
@@ -0,0 +1,104 @@
+import MDAnalysis as mda
+import numpy as np
+import argparse
+
+
+def calculate_angle(vec1, vec2):
+
+    dot_product = np.inner(vec1, vec2)
+
+    # Calculate the magnitudes (norms) of the vectors
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+
+    # Calculate the cosine of the angle
+    cos_angle = dot_product / (norm_vec1 * norm_vec2)
+
+    # Calculate the angle in radians
+    angle_radians = np.arccos(cos_angle)
+
+    # Convert the angle to degrees
+    # angle_degrees = np.degrees(angle_radians)
+
+    return angle_radians
+
+
+def get_vectors(protein, resolution='cg'):
+    select_atomistic = {
+    'A1': 'chainID A and resid 108 97 and name CA',
+    'A2': 'chainID A and resid 108 43 and name CA',
+    'B1': 'chainID B and resid 108 97 and name CA',
+    'B2': 'chainID B and resid 108 43 and name CA',
+    }
+    select_cg = {
+    'A1': 'chainID A and resid 98 87 and name BB',
+    'A2': 'chainID A and resid 98 42 and name BB',
+    'B1': 'chainID B and resid 98 87 and name BB',
+    'B2': 'chainID B and resid 98 42 and name BB',
+    }
+
+    if resolution == 'atomistic': 
+        selection = select_atomistic
+    elif resolution == 'cg':
+        selection = select_cg
+
+    vectors = {}
+
+    for k, v in selection.items():
+        s = protein.select_atoms(v).positions
+        s = s[1] - s[0]
+        s = s / np.linalg.norm(s)
+        vectors[k] = s 
+
+    return vectors.values()
+
+
+def print_progress(i, traj_size):
+    progress_percent = 100 * i/traj_size
+    print(f"Progress: {progress_percent:.1f}%", end='\r')
+
+
+# Parse command line
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f",  help="Trajectory file", dest='trajectory', type=str)
+parser.add_argument("-s", help="Topology file (Should be a .pdb or .tpr)", dest='topology', type=str)
+parser.add_argument("-res", help="Resolution (Options: 'cg' or 'atomistics')", dest='resolution', type=str)
+parser.add_argument("-o", help="Output file", dest='out_file', type=str)
+
+args = parser.parse_args()
+
+
+topology_file = args.topology  # f'{dir}conf.pdb'
+trajectory_file = args.trajectory  # f'{dir}center.xtc'
+u = mda.Universe(topology_file, trajectory_file)
+
+protein = u.select_atoms('not resname DPPG DPPC W')
+
+
+traj = u.trajectory[0:-1]
+traj_size = len(traj)
+time_series = np.zeros((2, traj_size))
+
+
+for i, ts in enumerate(traj):
+    A1, A2, B1, B2 = get_vectors(protein, args.resolution)
+
+    angle_A = calculate_angle(B1, A1)
+    angle_T = calculate_angle(np.cross(B2, B1), np.cross(A1, A2))
+
+    time_series[0][i] = np.rad2deg(angle_A)
+    time_series[1][i] = np.rad2deg(angle_T)
+
+    print_progress(i, traj_size)
+
+
+np.savetxt(
+    args.out_file, 
+    time_series.T, 
+    header=f'Theta A\tTheta T', 
+    delimiter=f'\t',
+    fmt='%.5e'
+)
+
+
